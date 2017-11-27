7eb1c6f66fb66bc999023ca150100e1405311fb0
gabrielknust <gabrielsilvaknustws@hotmail.com>
2017-11-27 10:10:01 -0200

    Vinal fersion

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
 			shuffle(a);
 			console.log(a);
 			var que = [];
-			var question=1;
-			for(var imga=0;imga<=10;imga++){
+			var question=0;
+			for(var imga=0;imga<10;imga++){
 				$.each(questoes,function(id,obj){
 					if(a[imga]==obj.id)
 					{
@@ -31,33 +31,25 @@
 						$(".pergunta").append($("<li />").addClass("perguntas").addClass("pergunta_item"+question).attr("id","pergunta_item"+question).append($("<a />").attr("href","#pergunta_item"+antes).append($("<i />").addClass("fa fa-chevron-left").attr("aria-hidden","true"))).append($("<img />").attr("src",obj.foto)).append($("<a />").attr("href","#pergunta_item"+depois).append($("<i />").addClass("fa fa-chevron-right").attr("aria-hidden","true"))));
 						que.push(obj.alternativas);
 						question++;
-						//for(var q=0;q<que.length;q++){
-						//	$(".pergunta_item").append($("<div />").addClass("option").append($("<label />").html(que[q]).append($("<input />").attr("type","radio").attr("name","question-"+imga+"-answer").attr("value",String.fromCharCode(test)))));
-						//	test++;
-						//};
 					}
-
 				});
 			}
-
-		for(question=1;question<11;question++){
-			console.log(que[question]);
+		for(question=0;question<10;question++){
 			var test=65;
-			for(var y=0;y<=que[question];y++){
-			$(".pergunta_item"+question).append($("<div />").addClass("option").append($("<label />").html("Alternativa "+String.fromCharCode(test)).append($("<input />").attr("type","radio").attr("name","question-"+question+"-answer").attr("value",String.fromCharCode(test)))));
+			for(var y=0;y<que[question];y++){
+			$("#pergunta_item"+question).append($("<div />").addClass("option").append($("<input />").attr("type","radio").attr("name","question-"+question+"-answers").attr("value",String.fromCharCode(test)+"-"+a[question])).append($("<label />").html("Alternativa "+String.fromCharCode(test))));
 			test++;
 		}
 		}
-
 	});
 	</script>
 </head>
 <body>
+	<form action="processa.php" method="post" id="quiz">
 	<div class="page-wrap">
 		<h1>Quízmica</h1>
-		<form action="processa.php" method="post" id="quiz">
 			<input type="text" name="nomeusuario" class="nomeusuario" placeholder="Seu nome" > <!--required oninvalid="this.setCustomValidity('Por favor, insira um nome!')"-->
-			<a href="#pergunta_item1" id="linkPergunta"><p>Ir para perguntas</p></a>
+			<a href="#pergunta_item0" id="linkPergunta"><p>Ir para perguntas</p></a>
 	</div>
 	<section class="conteiner-perguntas">
 			<ul class="pergunta" id="pergunta">
--- processa.php
+++ processa.php
@@ -6,8 +6,9 @@
 		<title>PHP Quiz</title> 
 		<link rel="stylesheet" type="text/css" href="./css/estiloprocessa.css" /> 
 		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+		<script src="./js/jquery.min.js"></script>
+		<script type="text/javascript" src="./js/json.js"></script>
 </head> 
-
 <body> 
 	
 	<div class="page-wrap"> 
@@ -15,13 +16,14 @@
 		<h1>Resultado:</h1> 
 
 		<?php 
-		if(!isset($_POST['question-1-answers'])||!isset($_POST['question-2-answers'])||!isset($_POST['question-3-answers'])||!isset($_POST['question-4-answers'])||!isset($_POST['question-5-answers'])||!isset($_POST['question-6-answers'])||!isset($_POST['question-7-answers'])||!isset($_POST['question-8-answers'])||!isset($_POST['question-9-answers'])||!isset($_POST['question-10-answers'])){
+		if(!isset($_POST['question-0-answers'])||!isset($_POST['question-1-answers'])||!isset($_POST['question-2-answers'])||!isset($_POST['question-3-answers'])||!isset($_POST['question-4-answers'])||!isset($_POST['question-5-answers'])||!isset($_POST['question-6-answers'])||!isset($_POST['question-7-answers'])||!isset($_POST['question-8-answers'])||!isset($_POST['question-9-answers'])){
 			echo "Erro, você não preencheu todas as respostas!";
 			?>
 			<br><div class="link"><button onclick="history.go(-1);"><p>Voltar</p></button></div>
 			<?php
 		}
 		else {
+		$answer0 = $_POST['question-0-answers'];
 		$answer1 = $_POST['question-1-answers'];
 		$answer2 = $_POST['question-2-answers'];
 		$answer3 = $_POST['question-3-answers'];
@@ -31,38 +33,151 @@
 		$answer7 = $_POST['question-7-answers'];
 		$answer8 = $_POST['question-8-answers'];
 		$answer9 = $_POST['question-9-answers'];
-		$answer10 = $_POST['question-10-answers'];
 		$nome = $_POST['nomeusuario'];
 		$totalCorrect = 0; 
+		$answer0=explode("-",$answer0);
+		$answer1=explode("-",$answer1);
+		$answer2=explode("-",$answer2);
+		$answer3=explode("-",$answer3);
+		$answer4=explode("-",$answer4);
+		$answer5=explode("-",$answer5);
+		$answer6=explode("-",$answer6);
+		$answer7=explode("-",$answer7);
+		$answer8=explode("-",$answer8);
+		$answer9=explode("-",$answer9);
 		$a=0;
 		$correct=array();
 		$wrong=array();
 		$w=0;
-		if ($answer1 == "E") { $totalCorrect++; $correct[$a]=1; $a++;} else{$wrong[$w]="1";$w++;}
-		if ($answer2 == "D") { $totalCorrect++; $correct[$a]=2; $a++;} else{$wrong[$w]="2";$w++;}
-		if ($answer3 == "C") { $totalCorrect++; $correct[$a]=3; $a++;} else{$wrong[$w]="3";$w++;}
-		if ($answer4 == "B") { $totalCorrect++; $correct[$a]=4; $a++;} else{$wrong[$w]="4";$w++;}
-		if ($answer5 == "C") { $totalCorrect++; $correct[$a]=5; $a++;} else{$wrong[$w]="5";$w++;}
-		if ($answer6 == "E") { $totalCorrect++; $correct[$a]=6; $a++;} else{$wrong[$w]="6";$w++;}
-		if ($answer7 == "C") { $totalCorrect++; $correct[$a]=7; $a++;} else{$wrong[$w]="7";$w++;}
-		if ($answer8 == "A") { $totalCorrect++; $correct[$a]=8; $a++;} else{$wrong[$w]="8";$w++;}
-		if ($answer9 == "A") { $totalCorrect++; $correct[$a]=9; $a++;} else{$wrong[$w]="9";$w++;}
-		if ($answer10 == "B") { $totalCorrect++; $correct[$a]=10; $a++;} else{$wrong[$w]="10";$w++;}
-		if($totalCorrect==0){
-			echo "<div class='results'> $nome, você não acertou nenhuma questão!</div>";
-		} 
-
-		echo "<div class='results'> $nome, você acertou $totalCorrect de 10 perguntas</div><br><p class='results'>Corretas:</p>
-		<div class='results'>";
-		for($x=0;$x<$a;$x++){
-			echo "$correct[$x]<br>";
-		}
-		echo "Erradas:<br>";
-		for($x=0;$x<$w;$x++){
-			echo "$wrong[$x]<br>";
-		}
-			echo "</div>";
-		
+		$certas=array();
+		?>
+		<script>
+			$(function(){
+				function vaitomarnocu(id){
+					var vtnc=1;
+					$.each(questoes,function(ind,obj){
+						if(obj.id==id){
+							vtnc=obj.correta;
+						}
+					});
+				return vtnc;
+				}
+				var answer0= "<?php echo $answer0[1]; ?>" ;
+				var corr0= "<?php echo $answer0[0]; ?>";
+				var answer1= "<?php echo $answer1[1]; ?>" ;
+				var corr1= "<?php echo $answer1[0]; ?>";
+				var answer2= "<?php echo $answer2[1]; ?>" ;
+				var corr2= "<?php echo $answer2[0]; ?>";
+				var answer3= "<?php echo $answer3[1]; ?>" ;
+				var corr3= "<?php echo $answer3[0]; ?>";
+				var answer4= "<?php echo $answer4[1]; ?>" ;
+				var corr4= "<?php echo $answer4[0]; ?>";
+				var answer5= "<?php echo $answer5[1]; ?>" ;
+				var corr5= "<?php echo $answer5[0]; ?>";
+				var answer6= "<?php echo $answer6[1]; ?>" ;
+				var corr6= "<?php echo $answer6[0]; ?>";
+				var answer7= "<?php echo $answer7[1]; ?>" ;
+				var corr7= "<?php echo $answer7[0]; ?>";
+				var answer8= "<?php echo $answer8[1]; ?>" ;
+				var corr8= "<?php echo $answer8[0]; ?>";
+				var answer9= "<?php echo $answer9[1]; ?>" ;
+				var corr9= "<?php echo $answer9[0]; ?>";
+				var nome="<?php echo $nome; ?>";
+				console.log(corr0);
+				console.log(answer0);
+				console.log(corr1);
+				console.log(answer1);
+				console.log(corr2);
+				console.log(answer2);
+				console.log(corr3);
+				console.log(answer3);
+				console.log(corr4);
+				console.log(answer4);
+				console.log(corr5);
+				console.log(answer5);
+				console.log(corr6);
+				console.log(answer6);
+				console.log(corr7);
+				console.log(answer7);
+				console.log(corr8);
+				console.log(answer8);
+				console.log(corr9);
+				console.log(answer9);
+				var totalCorrect=0;
+				correta=vaitomarnocu(answer0);
+				if(correta==corr0){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer1);
+				if(correta==corr1){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer2);
+				if(correta==corr2){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer3);
+				if(correta==corr3){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer4);
+				if(correta==corr4){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer5);
+				if(correta==corr5){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer6);
+				if(correta==corr6){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer7);
+				if(correta==corr7){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer8);
+				if(correta==corr8){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				correta=vaitomarnocu(answer9);
+				if(correta==corr9){
+					totalCorrect++;
+				}
+				else {
+					console.log("correta");
+				}
+				$(".page-wrap").append($("<div />").addClass("results").html(nome+",você acertou um total de "+totalCorrect+" de 10 questões")).append($("<a />").attr("href","ranking.php").append($("<div />").addClass("link").append($("<p />").html("Confira o ranking aqui e veja como as outras pessoas se saíram"))));
+			});
+		</script>
+		<?php
 			$caminhoArquivo="./documents/ranking.txt";
 
 			$handle=fopen($caminhoArquivo, "a");
@@ -74,7 +189,6 @@
 				fwrite($handle,PHP_EOL.$nome.";".$totalCorrect);
 			}
 		?> 
-		<a href="ranking.php"><div class="link"><p> Confira o ranking aqui e veja como as outras pessoas se saíram</p></div></a> 
 		<?php } ?>
 	</div> 
 
